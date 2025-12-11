4cb2e8492f16117144baed6cc9501a77abf1db43
Navanath Bhosale <navanathbhosle29@gmail.com>
2025-12-11 09:31:28 +0530

    first commit

---
+++ program.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    return a + b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+def subtract(a,b):
+    return a - b
